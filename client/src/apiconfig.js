e16ad116afe41a2e66c84fff85d757167c13018d
KyleKundinger <kundingerk@lakeland.edu>
2023-04-06 21:20:43 -0500

    added the number 1

--- client/src/apiconfig.js
+++ client/src/apiconfig.js
@@ -1,6 +1,6 @@
 const API_BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://kundingerschneiderwedding.onrender.com"
+    ? "https://kundingerschneiderwedding1.onrender.com"
     : "http://localhost:5000";
 
 export default API_BASE_URL;
