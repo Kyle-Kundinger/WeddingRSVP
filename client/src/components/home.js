9e05646291f3d3fdb818211f1bb7a5e379fa424d
KyleKundinger <kundingerk@lakeland.edu>
2023-04-08 11:29:11 -0500

    updated home page

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,18 +1,59 @@
 import React from "react";
+import Rsvp from "./rsvp";
 
-function Home() {
+class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showRsvp: false,
+    };
+    this.toggleRsvp = this.toggleRsvp.bind(this);
+  }
+
+  toggleRsvp() {
+    this.setState(prevState => ({ showRsvp: !prevState.showRsvp }));
+  }
+
+  render() {
+    const { showRsvp } = this.state;
+
     return (
-      <div>
-        <h1>Directions</h1>
-        <p>Here are the directions to our event:</p>
-        <ul>
-          <li>From the airport, take the highway heading west.</li>
-          <li>Exit at Main Street and turn right.</li>
-          <li>Go straight for 2 miles and turn left onto Elm Street.</li>
-          <li>Our event venue will be on your right.</li>
-        </ul>
+      <div
+        className="container"
+        style={{
+          paddingTop: "60px",
+          color: "white",
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+          marginTop: "40px",
+          boxShadow: "0 0 10px rgba(0, 0, 0, 1)",
+          borderRadius: "10px",
+        }}
+      >
+        {!showRsvp && (
+          <>
+            <h1>Welcome to Our Wedding!</h1>
+            <p>
+              We are thrilled to have you join us in celebrating our special day.
+              Please find the event details below:
+            </p>
+            <p style={{ marginLeft: '1em', lineHeight: '1.8em' }}>
+              Date: [Wedding Date]<br />
+              Time: [Wedding Time]<br />
+              Venue: [Wedding Venue]<br />
+              Address: [Wedding Venue Address]
+            </p>
+            <button onClick={this.toggleRsvp}>RSVP</button>
+          </>
+        )}
+        {showRsvp && (
+          <>
+            <Rsvp />
+            <button onClick={this.toggleRsvp}>Back to Home</button>
+          </>
+        )}
       </div>
     );
   }
+}
 
 export default Home;
--- client/src/components/rsvp.js
+++ client/src/components/rsvp.js
@@ -12,8 +12,6 @@ const Rsvp = () => {
   const [people, setPeople] = useState([]);
   const [invalid, setInvalid] = useState(false);
 
-  
-
   const handleChange = (event) => {
     setName(event.target.value);
   };
@@ -36,45 +34,26 @@ const Rsvp = () => {
   };
 
   return (
-    <div style={{
-      display: 'flex',
-      justifyContent: 'center',
-      minHeight: 'calc(100vh - 80px)',
-      padding: '0 20px'
-    }}>
-      <div style={{
-        position: 'relative',
-        marginTop: '80px',
-        backgroundColor: 'rgba(52, 58, 64, 0.6)',
-        padding: '20px',
-        borderRadius: '10px',
-        boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)',
-        zIndex: '999',
-        color: 'white',
-      }}>
-        {loading ? (
-          <Loading type="spin" color="#FFFFFF" />
-        ) : 
-        submitted ? (
-          <RsvpForm name={name} personData={personData} people={people} />
-        ) : (
-          <form onSubmit={handleSubmit}>
-            <h2>Please start the process by entering your group number</h2>
-            <label>
-              (This is the number that was sent to you in your invitation)
-              <br />
-              {invalid ? <span><br /><p>Sorry, we couldn't find that group number</p></span> : ''}
-              <br />
-              <input type="text" value={name} onChange={handleChange} />
-              
-            </label>
-            
+    <div>
+      {loading ? (
+        <Loading type="spin" color="#FFFFFF" />
+      ) : submitted ? (
+        <RsvpForm name={name} personData={personData} people={people} />
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <h2>Please start the process by entering your group number</h2>
+          <label>
+            (This is the number that was sent to you in your invitation)
             <br />
+            {invalid ? <span><br /><p>Sorry, we couldn't find that group number</p></span> : ''}
             <br />
-            <input type="submit" value={loading ? 'Loading...' : 'RSVP'} disabled={loading} />
-          </form>
-        )}
-      </div>
+            <input type="text" value={name} onChange={handleChange} />
+          </label>
+          <br />
+          <br />
+          <input type="submit" value={loading ? 'Loading...' : 'RSVP'} disabled={loading} />
+        </form>
+      )}
     </div>
   );
 };
